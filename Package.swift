7e49de33ef62ab3ccb6a9efaecf0042aecdccd2e
Rostyslav <>
2024-03-25 15:27:23 +0200

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
+
 import PackageDescription
 
 let package = Package(
   name: "BanubaPhotoEditorSDK",
   platforms: [
-    .iOS(.v14)
+    .iOS(.v15)
   ],
   products: [
     .library(
@@ -13,19 +14,19 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/sdk-banuba/BNBSdkCore.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBSdkApi.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBEffectPlayer.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBScripting.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBLips.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBHair.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBEyes.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBBackground.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBSkin.git", .exact("1.10.1")),
-    .package(url: "https://github.com/sdk-banuba/BNBAcneEyebagsRemoval.git", .exact("1.10.1")),
-    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", from: "1.33.0"),
-    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", from: "1.33.0"),
-    .package(url: "https://github.com/Banuba/BNBLicenseUtils-iOS.git", from: "1.33.0")
+    .package(url: "https://github.com/sdk-banuba/BNBSdkCore.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBSdkApi.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBEffectPlayer.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBScripting.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBLips.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBHair.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBEyes.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBBackground.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBSkin.git", .exact("1.11.1")),
+    .package(url: "https://github.com/sdk-banuba/BNBAcneEyebagsRemoval.git", .exact("1.11.1")),
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", from: "1.35.0"),
+    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", from: "1.35.0"),
+    .package(url: "https://github.com/Banuba/BNBLicenseUtils-iOS.git", from: "1.35.0")
   ],
   targets: [
     .target(
