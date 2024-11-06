5f5beea163985cefad6b386a5448eeb940ee43c0
user <user@users-Mac-mini.local>
2024-11-06 18:11:25 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let faceARversionRange: Range<Version> = "1.15.0"..<"1.15.99"
+let faceARversionRange: Range<Version> = "1.16.0"..<"1.16.99"
 let package = Package(
   name: "BanubaPhotoEditorSDK",
   platforms: [
